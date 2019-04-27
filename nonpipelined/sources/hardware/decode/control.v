c1f11ef7c902241dc6184feafaad5c90b2dc81df
Matthew Olsson <matthewcolsson@gmail.com>
2019-04-27 16:11:31 -0500

    Refactored control.v
    Defaulted all signals to zero and only set non-zero signals in the case blocks. It takes up more lines, but drastically improves readability

--- nonpipelined/sources/hardware/decode/control.v
+++ nonpipelined/sources/hardware/decode/control.v
@@ -4,44 +4,91 @@
 module control(
     input [10:0] opcode,
     output reg readreg2_control,
-    output reg mem_read,
-    output reg mem_write,
-    output reg mem_to_reg,
-    output reg reg_write,
-    output reg alu_src,
+               mem_read,
+               mem_write,
+               mem_to_reg,
+               reg_write,
+               alu_src,
+               write_reg_control,
+               update_sreg,
     output reg [1:0] alu_op,
-    output reg update_sreg,
     output reg [2:0] branch_op // 000 => no branch, 001 => branch, 010 => branch_conditionally, 011 => branch_if_zero, 100 => branch_if_not_zero
 );
     
     always @* begin
+        // Set every signal to zero
+        readreg2_control <= 0;
+        mem_read <= 0;
+        mem_write <= 0;
+        mem_to_reg <= 0;
+        reg_write <= 0;
+        alu_src <= 0;
+        alu_op <= 0;
+        update_sreg <= 0;
+        branch_op <= 0;
+        
+        // Set bits to 1
         casex(opcode)
-            `ADD, `SUB, `AND, `ORR:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_1_0_10_0_000;
-            `ADDS, `ANDS, `SUBS:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_1_0_10_1_000;
-            `ADDI, `ANDI, `EORI, `ORRI, `SUBI:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_1_1_10_0_000;
-            `ADDIS, `ANDIS, `SUBS:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_1_1_10_1_000;
-            `CMP:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_0_0_10_1_000;
-            `CMPI:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_0_1_10_1_000;
-            `LDUR, `LDURB, `LDURH, `LDURSW:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_1_0_1_1_1_00_0_000;
-            `STUR, `STURB, `STURH, `STURW:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b1_0_1_0_0_1_00_0_000;
-            `CBZ:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b1_0_0_0_0_0_01_1_011;
-            `CBNZ:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b1_0_0_0_0_0_01_1_100;
-            `B:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_0_0_01_0_001;
-            `BCOND:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_0_0_01_0_010;
-            default:
-                {readreg2_control, mem_read, mem_write, mem_to_reg, reg_write, alu_src, alu_op, update_sreg, branch_op} <= 'b0_0_0_0_0_0_00_0_000;
+            `ADD, `SUB, `AND, `ORR: begin
+                reg_write <= 1;
+                alu_op <= 'b10;
+            end
+            `ADDS, `ANDS, `SUBS: begin
+                reg_write <= 1;
+                alu_op <= 'b10;
+                update_sreg <= 1;
+            end
+            `ADDI, `ANDI, `EORI, `ORRI, `SUBI: begin
+                reg_write <= 1;
+                alu_src <= 1;
+                alu_op <= 'b10;
+            end
+            `ADDIS, `ANDIS, `SUBS: begin
+                reg_write <= 1;
+                alu_src <= 1;
+                alu_op <= 'b10;
+                update_sreg <= 1;
+            end
+            `CMP: begin
+                alu_op <= 'b10;
+                update_sreg <= 1;
+            end
+            `CMPI: begin
+                alu_src <= 1;
+                alu_op <= 'b10;
+                update_sreg <= 1;
+            end
+            `LDUR, `LDURB, `LDURH, `LDURSW: begin
+                mem_read <= 1;
+                mem_to_reg <= 1;
+                reg_write <= 1;
+                alu_src <= 1;
+            end
+            `STUR, `STURB, `STURH, `STURW: begin
+                readreg2_control <= 1;
+                mem_write <= 1;
+                alu_src <= 1;
+            end
+            `CBZ: begin
+                readreg2_control <= 1;
+                alu_op <= 'b01;
+                update_sreg <= 1;
+                branch_op <= 'b011;
+            end
+            `CBNZ: begin
+                readreg2_control <= 1;
+                alu_op <= 'b01;
+                update_sreg <= 1;
+                branch_op <= 'b100;
+            end
+            `B: begin
+                alu_op <= 'b01;
+                branch_op <= 'b001;
+            end
+            `BCOND: begin
+                alu_op <= 'b01;
+                branch_op <= 'b010;
+            end
         endcase
     end  
     
